adb02588e4949171e544189689adb7a2162754fb
Monika Myszkowska <monikamyszkowska01@gmail.com>
2022-07-21 15:27:18 +0200

    inicialize app

---
+++ Assets/VRLookWalk.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VRLookWalk : MonoBehaviour
+{
+    public Transform vrCamera;
+    public float toggleAngle = 20.0f;
+    public float speed = 3.0f;
+    public bool moveForward;
+    private CharacterController character;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(vrCamera.eulerAngles.x >= toggleAngle && vrCamera.eulerAngles.x < 90.0f)
+        {
+            moveForward = true;
+        }
+        else
+        {
+            moveForward = false;
+        }
+        if (moveForward)
+        {
+            Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+            character.SimpleMove(forward * speed);
+        }
+    }
+}
